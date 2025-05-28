d6deadd16f12fae9f6936aa1fee24cd3769b16c7
Izan de Geer <izandegeer.ua@gmail.com>
2025-05-28 16:48:21 +0200

    Add files via upload

---
+++ Unidad5/src/org/example/Television.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class Television {
+    private int canal;
+
+    public void Televisor() {
+    }
+
+    public void Televisor() {
+        int valorCanal;
+    }
+
+    public void subirCanal() {
+    }
+
+    public void bajarCanal() {
+    }
+
+    public void getcanal() {}
+
+    public void setCanal() {
+        int valorCanal;
+    }
+}
